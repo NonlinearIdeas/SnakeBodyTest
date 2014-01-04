6a3d1523752cc2ed39a810577d2b73cecf7b2ab3
James Wucher <james@nlideas4.gateway.2wire.net>
2014-01-03 18:46:55 -0600

    Created the initial project and added the snake code to it.
    This project demonstrates creating a moving snake body out of box2d
    bodies.  The snake moves around, dragged by its head.  It uses several
    components from other projects.

---
+++ SnakeBodyTest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SnakeBodyTestAppController.h
+//  SnakeBodyTest
+//
+//  Created by James Wucher on 1/3/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SnakeBodyTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SnakeBodyTestAppController.h
+//  SnakeBodyTest
+//
+//  Created by James Wucher on 1/3/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
